92487cbb32a95e3997e521763843acaac29b7fb8
y12studio <y12studio@gmail.com>
2018-11-11 22:53:26 +0800

    generate protobuffer doc

---
+++ openhcos/projects/wasm-red/README.md
@@ -0,0 +1,11 @@
+## Setup
+
+- https://rustwasm.github.io/wasm-bindgen/whirlwind-tour/introduction.html
+- https://rustwasm.github.io/book/game-of-life/setup.html#setup
+- Releases · protocolbuffers/protobuf 
+ https://github.com/protocolbuffers/protobuf/releases
+
+```
+cd openhcos/projects/wasm-hot
+./build.sh init
+```
---
+++ openhcos/projects/wasm-red/build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#set -x
+
+cd "$(dirname "$0")"
+WASM_FILE="wasm_hot.wasm"
+BINDGEN_DIR="bindout"
+
+init-rust() {
+    rustup target add wasm32-unknown-unknown --toolchain nightly
+    rustup update
+    rustup default nightly
+    cargo +nightly install wasm-bindgen-cli
+}
+
+init-protoc() {
+    # windows setup
+    # https://github.com/protocolbuffers/protobuf/issues/2698
+    echo "install protoc first"
+}
+
+proto-doc() {
+    # go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc
+    # windows platform
+    GOOGLE_INCUDE_PATH=C:/go/src
+    PROTOS=../../../proto/stu3/*.proto
+    protoc --doc_out=./doc --doc_opt=html,proto-fhir.html --proto_path=../../../ --proto_path=${GOOGLE_INCUDE_PATH} ${PROTOS}
+    protoc --doc_out=./doc --doc_opt=markdown,proto-fhir.md --proto_path=../../../ --proto_path=${GOOGLE_INCUDE_PATH} ${PROTOS}
+}
+
+server() {
+    # wasm mimetype is not supported by the lite-server command
+    npm run serve
+}
+
+build(){
+    mkdir -p ${BINDGEN_DIR}
+    cargo  +nightly build --target wasm32-unknown-unknown
+    wasm-bindgen target/wasm32-unknown-unknown/debug/${WASM_FILE} --out-dir ./${BINDGEN_DIR}
+}
+
+case "$1" in 
+    init)   init-rust ;;
+    build)   build ;;
+    doc)   proto-doc ;;
+    *) echo "usage: $0 start|stop|restart" >&2
+       exit 1
+       ;;
+esac
